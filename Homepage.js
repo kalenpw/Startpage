b1fe4a80ff1510946e12d35923e6b5e12a82f0fa
kalenpw <kalenpwilliams@gmail.com>
2017-12-18 13:39:07 -0700

    Initial commit

---
+++ Homepage.js
@@ -0,0 +1,236 @@
+"use strict";
+
+window.onload = function () {
+    //createQuickLinks();
+    var bodyElement = document.getElementById("body");
+    bodyElement.addEventListener("keypress", keypressed);
+
+    var h3s = document.getElementsByTagName("H3");
+    for (var i = 0; i < h3s.length; i++) {
+        h3s[i].addEventListener("click", handleH3Click);
+    }
+    updateImageSizes();
+    handleClock();
+    
+    getTopLevelQuickLinkArray();
+
+    var topLevelLinks = new Array();
+    for(var key in QuickLinkTypes){
+        topLevelLinks.push(document.getElementById(key));
+    }
+
+    for(let i = 0; i < topLevelLinks.length; i++){
+        topLevelLinks[i].addEventListener("click", handleQuickLinkClick);
+    }
+}
+
+window.onresize = updateImageSizes;
+
+//Updates the frequent link icon sizes
+function updateImageSizes(){
+    let bodyEle = document.getElementsByTagName("BODY")[0];
+    let screenWidth = bodyEle.offsetWidth;
+    //10% is margin of page
+    let tenPercent = screenWidth / 10;
+    //50 is arbitrary amount to remove to account for padding 
+    screenWidth = screenWidth - tenPercent - 50;
+    //10 total icons so divide by 10
+    let imgSize = screenWidth / 10;
+    //Don't let images over 150px exist
+    imgSize = (imgSize > 150) ? 150 : imgSize;
+    let styleFormatted = imgSize + "px";
+    let icons = document.getElementsByClassName("icon");
+    for(let i = 0; i < icons.length; i++){
+        icons[i].style.height = styleFormatted;
+        icons[i].style.width = styleFormatted;
+    }
+}
+
+//Display clock and keep updating every second
+function handleClock(){
+    setInterval(function(){
+        var options = {hour12: false};
+        var date = new Date();
+        var dayOfWeek = date.getDay();
+        var day = date.getDate();
+        var month = date.getMonth();
+        var year = date.getFullYear();
+
+        var daysOfWeek = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+        var months = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+
+        document.getElementById("date").innerHTML = daysOfWeek[dayOfWeek] + " " + year + " " + months[month] + " " + day
+            + "<br/>" + (new Date()).toLocaleTimeString('en-US', options);
+    }, 1000)
+}
+
+//Toggles hiding inner content of each h3 tag
+function handleH3Click(event) {
+    var clickedContent = event.target.innerText;
+    var elementToHide = getH3FromContents(clickedContent);
+
+    var style = window.getComputedStyle(elementToHide);
+    var display = style.getPropertyValue("display");
+
+    if (display === "inline") {
+        elementToHide.style.display = "none";
+    }
+    else {
+        elementToHide.style.display = "inline";
+    }
+}
+
+function handleQuickLinkClick(event){
+    let clickedContent = event.target.innerText;
+    let linksToShow = document.getElementsByClassName(clickedContent);
+    let style = window.getComputedStyle(event.target);
+    let clickedBackgroundColor = style.getPropertyValue("background-color");
+    
+    let lightBlueInRgb = "rgb(99, 217, 255)";
+    let blueInRgb = "rgb(68, 202, 246)";
+    let darkerBlueInRgb = "rgb(8, 130, 170)";
+
+
+    if(clickedBackgroundColor === blueInRgb || clickedBackgroundColor === lightBlueInRgb){
+        event.target.style.backgroundColor = darkerBlueInRgb;
+    }
+    else{
+        event.target.style.backgroundColor = blueInRgb + "!important";
+    }
+
+    for(let i = 0; i < linksToShow.length; i++){
+        let style = window.getComputedStyle(linksToShow[i]);
+        let display = style.getPropertyValue("display");
+
+        if(display === "none"){
+            linksToShow[i].style.display = "block";
+        }
+        else{
+            linksToShow[i].style.display = "none";
+        }
+//        linksToShow[i].removeClass("hidden");
+//        linksToShow[i].addClass("showLink");
+    }
+}
+
+//Returns whatever h3 tag has the matching text
+function getH3FromContents(str){
+    let id = "";
+    if (str === "Frequented") {
+        id = "freqLinks";
+    }
+    else if (str === "Quick Links") {
+        id = "quickLinkList";
+    }
+    else if (str === "//TODO") {
+        id = "todoDisplay";
+    }
+    else if(str === "Info 4430 Links"){
+        id = "homework";
+    }
+    return document.getElementById(id);
+}
+
+function getArrayFor(arrayType){
+    let array = new Array();
+    if(arrayType === QuickLinkTypes.School){
+        array.push(["https://bengalweb.isu.edu/cp/home/displaylogin", "Bengal Web"]);
+        array.push(["http://www2.cose.isu.edu/~edwajohn/teaching/2017fall/cs4481/index.html", " CS4481"]);
+    }
+    else if(arrayType === QuickLinkTypes.Work){
+        array.push(["https://cms.isu.edu/terminalfour/login.jsp", "T4 Login"]);
+        array.push(["https://community.terminalfour.com/info/api/terminalfour-8.0/index.html", "T4 Api"]);
+        array.push(["https://t4community.slack.com", "T4 Slack"]);
+        array.push(["https://selfservice.terminalfour.com/servicedesk/customer/portal/1", "T4 Support"]);
+        array.push(["https://cms.isu.edu/terminalfour/SiteManager?ctfn=query", "T4 SQL Query"]);
+    }
+    else if(arrayType === QuickLinkTypes.Programming){
+        array.push(["https://hub.spigotmc.org/javadocs/bukkit/", "Bukkit API"]);
+        array.push(["http://stackexchange.com/", "Stack Exchange"]);
+        array.push(["https://gitlab.com", "GitLab"]);
+        array.push(["https://code.excelereight.tk/wrcwpk", "Excel Git"]);
+        array.push(["http://kalenpw.com:81", "Dev Server"]);
+    }
+    else if(arrayType === QuickLinkTypes.Misc){
+        array.push(["https://www.thepiratebay.se", "Pirate Bay"]);
+        array.push(["https://mail.protonmail.com/login", "Proton Mail"]);
+        array.push(["https://animebytes.tv", "Animebytes"]);
+        array.push(["http://www.amazon.com", "Amazon"]);
+        array.push(["http://xkcd.com/", "XKCD"]);
+        array.push(["https://redacted.ch/", "Redacted"]);
+        array.push(["https://www.imgur.com", "Imgur"]);
+        array.push(["https://www.isucu.org/", "ISU CU"]);
+    }
+
+    array.sort(function (a, b){
+        var one = a[1];
+        var two = b[1];
+
+        if(one < two){
+            return -1;
+        }
+        if(one > two){
+            return 1;
+        }
+        return 0;
+    });
+
+    return array;
+}
+
+
+function formatQuickLink(url, name, className){
+    return '<a href="' + url + '" class="quickLink hidden innerLink ' + className + '">' + name + '</a>';
+}
+
+function formatTopLevelLink(name){
+    return "<li id='" + name + "' class='quickLink'>" + name + "</li>";
+}
+
+function getTopLevelQuickLinkArray(){
+    let formatted = "";
+    for(var key in QuickLinkTypes){
+        formatted += formatTopLevelLink(key);
+        let linkArray = getArrayFor(key);
+        for(let i = 0; i < linkArray.length; i++){
+            formatted += formatQuickLink(linkArray[i][0], linkArray[i][1], key);
+        }
+    }
+    document.getElementById("quickLinkList").innerHTML = formatted;
+}
+
+//Handles key presses
+function keypressed(event) {
+    var key = event.keyCode;
+    var keyBinds = createKeyBindArray();
+
+    for (var i = 0; i < keyBinds.length; i++) {
+        if (keyBinds[i][0] === key) {
+            window.open(keyBinds[i][1], "_self");
+        }
+    }
+}
+
+//Creates and fills the keybinding array
+function createKeyBindArray() {
+    var keyBinds = [
+        [49, "http://4chan.org/vg/vsg"],
+        [50, "http://kalenpw.com:8096"],
+        [51, "https://mail.google.com"],
+        [52, "https://github.com/kalenpw"],
+        [53, "http://kalenpw.com"],
+        [54, "http://elearn.isu.edu"],
+        [55, "https://www.netflix.com"],
+        [56, "https://www.reddit.com"],
+        [57, "http://www.twitter.com"],
+        [48, "https://www.youtube.com"]
+    ];
+    return keyBinds;
+}
+
+var QuickLinkTypes = Object.freeze({
+    School: 'School',
+    Work: 'Work',
+    Programming: 'Programming',
+    Misc: 'Misc'
+});
---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE HTML>
+<HTML>
+	<head>
+		<title>
+		Homepage
+        </title>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+		<script src="Homepage.js"></script>
+		<link href='http://fonts.googleapis.com/css?family=Gloria+Hallelujah' rel='stylesheet' type='text/css'>
+        <link href="https://fonts.googleapis.com/css?family=Baloo+Bhaijaan" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- TODO open links on keypress  -->
+	</head>
+	
+	<body id="body" onkeydown="keypressed(event)">
+        <h6 id="date">
+            <?php
+                //echo (date("l Y-m-d") . "<br/>" . date("H:i:s")) ;
+            ?>
+        </h6>
+        <br>
+
+		<h3>
+		Frequented
+		</h3>
+        <div id="freqLinks">
+    		<div>
+    			<p><span>1</span></p>
+    			<a href="https://4chan.org/vg/vsg">
+    				<img class="icon" src="Images/4chan.png" width="100px" height="100px" alt="Facebook">
+    			</a>
+    		</div>
+            <div>
+    			<p><span>2</span></p>
+    			<a href="http://kalenpw.com:8096">
+    				<img class="icon" src="Images/emby.png" width="100px" height="100px" alt="Emby">
+    			</a>
+    		</div>
+    		<div>
+    			<p><span>3</span></p>
+    			<a href="https://mail.google.com/mail/">
+    				<img class="icon" src="Images/gmail.png" width="100px" height="100px" alt="Gmail">
+    			</a>
+    		</div>
+            <div>
+                <p><span>4</span></p>
+                <a href="https://github.com/kalenpw">
+                    <img class="icon" src="Images/github.png" width="100px" height="100px" alt="GitHub">
+                </a>
+            </div>
+    		<div>
+    			<p><span>5</span></p>
+    			<a href="http://kalenpw.com">
+    			    <img class="icon" src="Images/khalidor.png" width="100px" height="100px" alt="Khalidor">
+    			</a>
+    		</div>
+    		<div>
+    			<p><span>6</span></p>
+    			<a href="http://elearn.isu.edu">
+    				<img class="icon" src="Images/moodle.png" width="100px" height="100px" alt="Moodle">
+    			</a>
+    		</div>
+    		<div>
+    			<p><span>7</span></p>
+    			<a href="http://www.netflix.com">
+    				<img class="icon" src="Images/netflix.png" width="100px" height="100px" alt="Netflix">
+    			</a>
+    		</div>
+    		<div>
+    			<p><span>8</span></p>
+    			<a href="http://www.reddit.com">
+    				<img class="icon" src="Images/reddit.png" width="100px" height="100px" alt="Reddit">
+    			</a>
+    		</div>
+    		<div>
+    			<p><span>9</span></p>
+    			<a href="http://www.twitter.com">
+    				<img class="icon" src="Images/twitter.png" width="100px" height="100px" alt="Twitter">
+    			</a>
+    		</div>
+    		<div>
+    			<p><span>0</span></p>
+    			<a href="http://www.youtube.com">
+    				<img class="icon" src="Images/youtube.png" width="100px" height="100px" alt="Youtube">
+    			</a>
+    		</div>
+        </div>
+		
+		<h3>
+		Quick Links
+		</h3>
+		<div id="quickLinkList">
+
+        </div>
+        
+        <h3>
+            //TODO
+        </h3>
+        
+        <div id="todoDisplay">
+        <ul id="todoList">
+        <?php
+            $todoFile = fopen('./todo.txt', 'r');
+            while($line = fgets($todoFile)) {
+                $formatted = '<li class="todoItem">' . $line . '</li>';
+                echo $formatted;
+                //echo $line;
+                
+            }
+            fclose($todoFile);
+
+        ?>
+        </ul>
+        </div>
+        
+        <h3>Info 4430 Links</h3>
+        <div id="homework">
+            <?php
+                  
+                $dir = "./Homework/";
+                if(is_dir($dir)){
+                    if($dh = opendir($dir)){
+                        while(($file = readdir($dh)) !== false){
+                            if(is_file("Homework/".$file)){
+                                if(strlen($file) > 3){
+                                    echo(createHwLink($file));
+                                }
+                            }
+                        }
+                        closedir($dh);
+                    }
+                }
+                function createHwLink($url){
+                    $formatted = "<a href='./Homework/" . $url . "' class='quickLink'>" . $url . "</a>";
+                    return $formatted;
+                }
+            ?>
+        </div>
+
+
+	</body>
+</HTML>
