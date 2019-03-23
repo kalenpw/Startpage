3ec3dfc689c70ab5d6402ed00591924f9011f883
kalenpw <kalenpwilliams@gmail.com>
2019-03-23 12:46:07 -0600

    Scrollbar style

--- src/App.vue
+++ src/App.vue
@@ -22,4 +22,31 @@ export default {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
 }
+
+/**************
+ * SCROLL BAR *
+ **************/
+::-webkit-scrollbar {
+    width: 12px;
+    background:#222;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3); 
+    -webkit-border-radius: 10px;
+    border-radius: 10px;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+    -webkit-border-radius: 10px;
+    border-radius: 10px;
+    background: rgba(15,15,15,0.8); 
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.5); 
+}
+::-webkit-scrollbar-thumb:window-inactive {
+	background: rgba(15,15,15,0.4); 
+}
+
 </style>
