61dcfef82829a01daad2a5c4f28b36c8cc112639
kalenpw <kalenpwilliams@gmail.com>
2021-05-27 23:05:54 -0600

    Generic browser scrollbar colors

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,13 @@ import EventBus from "@/eventbus.js";
 export default {
     name: "app",
     components: {
-        Root
+        Root,
     },
     created() {
-        window.addEventListener("keyup", function(event) {
+        window.addEventListener("keyup", function (event) {
             EventBus.$emit("keypress", event);
         });
-    }
+    },
 };
 </script>
 
@@ -33,18 +33,18 @@ export default {
 /**************
  * SCROLL BAR *
  **************/
+
+/* chrome */
 ::-webkit-scrollbar {
     width: 12px;
     background: #222;
 }
-
 /* Track */
 ::-webkit-scrollbar-track {
     -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
     -webkit-border-radius: 10px;
     border-radius: 10px;
 }
-
 /* Handle */
 ::-webkit-scrollbar-thumb {
     -webkit-border-radius: 10px;
@@ -54,5 +54,10 @@ export default {
 }
 ::-webkit-scrollbar-thumb:window-inactive {
     background: rgba(15, 15, 15, 0.4);
+}
+
+/* firefox */
+html {
+    scrollbar-color: #000 #222;
 }
 </style>
